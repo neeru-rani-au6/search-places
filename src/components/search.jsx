f70779f70c4a310593cf365670bfd165a8526484
neeru-rani-au6 <57033296+neeru-rani-au6@users.noreply.github.com>
2024-09-11 21:44:30 +0530

    active search box with sort key

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,12 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './search.css';
 
 const Search = ({ searchText, handleSearchTextChange }) => {
   const [isFocused, setIsFocused] = useState(false);
+  const searchInputRef = useRef(null);
+
 
   const handleInputChange = (e) => {
     handleSearchTextChange(e.target.value)
   };
+
+  // Focus the search input when Ctrl + / is pressed
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.ctrlKey && e.key === '/') {
+        e.preventDefault(); // Prevent the default action of Ctrl + /
+        if (searchInputRef.current) {
+          searchInputRef.current.focus();
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
 
   return (
     <div className="search-box-container">
@@ -15,11 +36,13 @@ const Search = ({ searchText, handleSearchTextChange }) => {
         className={`search-box`}
         placeholder="Search places..."
         value={searchText}
+        ref={searchInputRef}
         onChange={handleInputChange}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
         disabled={false} // Set this to true if you want to disable it
       />
+      <span className="keyboard-shortcut">Ctrl + /</span>
     </div>
   );
 };
